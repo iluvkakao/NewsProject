e8d8121c3b90899f67201415e2f081329f0b91ac
Nat Askerka <geeliat@gmail.com>
2021-09-25 12:30:43 +0300

    lottie changing

--- NewsProject/FirstThemeViewController.swift
+++ NewsProject/FirstThemeViewController.swift
@@ -16,22 +16,41 @@ final class FirstThemeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .black
        
-        setupAnimation()
-        let secondsToDelay = 2.0
-        DispatchQueue.main.asyncAfter(deadline: .now() + secondsToDelay) {
-            self.nextController()
-            
-        }
+//        setupAnimation()
+//        let secondsToDelay = 2.0
+//        DispatchQueue.main.asyncAfter(deadline: .now() + secondsToDelay) {
+//            self.nextController()
+//
+//        }
     }
-
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        setupAnimation()
+//
+//    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        setupAnimation()
+    }
+    
+    //viewdidAppear
     private func setupAnimation(){
         animationView.animation = Animation.named("news-app-preloader")
         animationView.frame = view.bounds
+//        animationView.clipsToBounds = true
+        animationView.center = view.center
         animationView.backgroundColor = .black
-        animationView.contentMode = .scaleAspectFill
-        animationView.loopMode = .loop
-        animationView.play()
+        animationView.contentMode = .scaleAspectFit
+//        animationView.loopMode = .loop
         view.addSubview(animationView )
+        // !! WEAK SELF
+        
+        animationView.play { [weak self] success in
+            self?.nextController()
+        }
+      
     }
     
     private func nextController(){
